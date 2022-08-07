7c5333cbd2f287a1dc82b1d60ee83f9359c07b09
Taimur Moolji <Taimur Moolji>
2022-08-07 18:41:07 -0400

    Static calendar page + form

--- weekify/src/input_form/input_form_base.js
+++ weekify/src/input_form/input_form_base.js
@@ -4,18 +4,27 @@ import cloneDeep from 'lodash/cloneDeep';
 
 function InputForm() {
 
+  const [task, setTask] = useState("")
+  const [time, setTime] = useState("")
+
   const form_submitted = () => {
     // console.log(arrayDisplay(gotten_time))
     // console.log(arrayDisplay(gotten_tasks))
 
     // console.log(task);
-    // console.log(time)
+    // console.log(time);
+    var the_task = cloneDeep(task);
+    gotten_tasks.push(the_task);
+    ttask = task;
+
+    setTask("");
+    setTime("");
   }
 
   return (
     <div className="Form center">
 
-      <form onSubmit={}>
+      <form onSubmit={form_submitted}>
         <div className="form-group">
           <p><strong>Day</strong></p>
           <input
@@ -24,6 +33,8 @@ function InputForm() {
               className="data-get"
               placeholder="Input the day of your task"
               maxLength="30"
+              onChange={event => setTask(event.target.value)}
+              value={task}
           />
         </div>
         <div className="form-group">
@@ -34,7 +45,9 @@ function InputForm() {
               className="data-get"
               placeholder="Input the time of your task {Time:Time - Time:Time}"
               maxLength="30"
-  
+              onChange={event => setTime(event.target.value)}
+              value={time}
+
         />
         </div>
         <div className="form-group">
@@ -45,13 +58,14 @@ function InputForm() {
               className="data-get"
               placeholder="Input your task in less than x characters"
               maxLength="30"
+              onChange={event => setTime(event.target.value)}
+              value={time}
 
-  
           />
         </div>
         <br/>
         <button type="submit" className="submit-button">Submit</button>
-        
+
       </form>
     </div>
   );
