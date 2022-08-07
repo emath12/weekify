98ef146029cdb604c6a9c8f8cd08659f0d8d10c6
emath12 <95326225+emath12@users.noreply.github.com>
2022-08-07 15:26:34 -0400

    Update input_form_base.js

--- input_form_base.js
+++ input_form_base.js
@@ -4,27 +4,18 @@ import cloneDeep from 'lodash/cloneDeep';
 
 function InputForm() {
 
-  const [task, setTask] = useState("")
-  const [time, setTime] = useState("")
-
   const form_submitted = () => {
     // console.log(arrayDisplay(gotten_time))
     // console.log(arrayDisplay(gotten_tasks))
 
     // console.log(task);
-    // console.log(time);
-    var the_task = cloneDeep(task);
-    gotten_tasks.push(the_task);   
-    ttask = task;
-    
-    setTask("");
-    setTime("");
+    // console.log(time)
   }
 
   return (
     <div className="Form center">
 
-      <form onSubmit={form_submitted}>
+      <form onSubmit={}>
         <div className="form-group">
           <p><strong>Day</strong></p>
           <input
@@ -33,8 +24,6 @@ function InputForm() {
               className="data-get"
               placeholder="Input the day of your task"
               maxLength="30"
-              onChange={event => setTask(event.target.value)}
-              value={task}
           />
         </div>
         <div className="form-group">
@@ -45,8 +34,6 @@ function InputForm() {
               className="data-get"
               placeholder="Input the time of your task {Time:Time - Time:Time}"
               maxLength="30"
-              onChange={event => setTime(event.target.value)}
-              value={time}
   
         />
         </div>
@@ -58,8 +45,7 @@ function InputForm() {
               className="data-get"
               placeholder="Input your task in less than x characters"
               maxLength="30"
-              onChange={event => setTime(event.target.value)}
-              value={time}
+
   
           />
         </div>
