23b26f97f157e5cf82031b1f09e4280e5c1aed3b
Taimur Moolji <Taimur Moolji>
2022-08-15 01:15:25 -0400

    fixed spacing issue and styled form

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import './App.css';
+// import "./debug.css";
 import ReactDOM from 'react-dom/client';
 
-// import './debug.css';
 
 var dict = {
-  "Monday" : "0/1",  
+  "Monday" : "0/1",
   "Tuesday" : "1/2",
   "Wednesday": "2/3",
   "Thursday" : "3/4",
-  "Friday" : "4/5",  
-  "8:00AM" : "0",
-  "8:30AM": "1",
-  "9:00AM" : "2",
-  "9:30AM": "3",
-  "10:00AM" : "4",
-  "10:30AM": "5",
-  "11:00AM" : "6",
-  "11:30AM": "7",
-  "12:00PM" : "8",
-  "12:30PM": "9",
-  "1:00PM" : "10",
-  "1:30PM": "11",
-  "2:00PM" : "12",
-  "2:30PM": "13",
-  "3:00PM" : "14",
-  "3:30PM": "15",
-  "4:00PM" : "16",
-  "4:30PM": "17",
-  "5:00PM" : "18",
-  "5:30PM": "19",
-  "6:00PM" : "20",
-  "6:30PM": "21",
-  "7:00PM" : "22",
-  "7:30PM": "23",
-  "8:00PM": "24",
+  "Friday" : "4/5",
+  "8:00AM" : "1",
+  "8:30AM": "2",
+  "9:00AM" : "3",
+  "9:30AM": "4",
+  "10:00AM" : "5",
+  "10:30AM": "6",
+  "11:00AM" : "7",
+  "11:30AM": "8",
+  "12:00PM" : "9",
+  "12:30PM": "10",
+  "1:00PM" : "11",
+  "1:30PM": "12",
+  "2:00PM" : "13",
+  "2:30PM": "14",
+  "3:00PM" : "15",
+  "3:30PM": "16",
+  "4:00PM" : "17",
+  "4:30PM": "18",
+  "5:00PM" : "19",
+  "5:30PM": "20",
+  "6:00PM" : "21",
+  "6:30PM": "22",
+  "7:00PM" : "23",
+  "7:30PM": "24",
+  "8:00PM": "25",
 };
 
 
@@ -85,7 +85,7 @@ class App extends React.Component {
       }
 
     }
-  
+
   onSubmit = event => {
     var pick;
     event.preventDefault();
@@ -95,7 +95,7 @@ class App extends React.Component {
     } else if (this.day.value == "Tuesday") {
       pick = this.state.tuesday;
     } else if (this.day.value == "Wednesday")  {
-      pick = this.state.wednesday;   
+      pick = this.state.wednesday;
     } else if (this.day.value == "Thursday") {
       pick = this.state.thursday;
     } else {
@@ -115,7 +115,7 @@ class App extends React.Component {
     this.setState({
       pick: data
     })
-  
+
    };
 
   render() {
@@ -164,37 +164,37 @@ class App extends React.Component {
 
             <div className = "days">
               <div className = "day" style={{backgroundColor: "rgb(204, 219, 220, 0.5)"}}>
-                  {                      
+                  {
                     this.state.monday.map((info, index) => {
                       console.log("info when passed: " + info.day);
                       if((info.day !== '') && (info.task !== '') && (info.start_time !== '') && (info.end_time !== '')) {
                           return (
-                            <div style={{gridColumnStart: dict[info.start_time], gridColumnEnd: dict[info.end_time]}}>
-                              <Card key={index} info={info}/>
-                            </div>
+                            <monday style={{gridRow: "" +  dict[info.start_time] + "/" + dict[info.end_time]}}>
+                              {info.task}
+                            </monday>
                           )
                         }
-                      } 
+                      }
                     )
                   }
               </div>
 
               <div className = "day" style={{backgroundColor: "rgb(154, 209, 212,0.5)"}}>
                     {
-                      
+
                       this.state.tuesday.map((info, index) => {
-          
+
                         if((info.day !== '') && (info.task !== '') && (info.start_time !== '') && (info.end_time !== '')) {
                             return (
-                              <div style={{gridColumnStart: dict[info.start_time], gridColumnEnd: dict[info.end_time]}}>
-                                <Card key={index} info={info}/>
-                              </div>
+                              <tuesday style={{gridRow: "" +  dict[info.start_time] + "/" + dict[info.end_time]}}>
+                                {info.task}
+                              </tuesday>
                             )
                           }
                         else {
                           console.log("invalid");
                         }
-                        } 
+                        }
                       )
                     }
               </div>
@@ -204,28 +204,28 @@ class App extends React.Component {
                         this.state.wednesday.map((info, index) => {
                           if((info.day !== '') && (info.task !== '') && (info.start_time !== '') && (info.end_time !== '')) {
                               return (
-                                <div style={{gridColumnStart: dict[info.start_time], gridColumnEnd: dict[info.end_time]}}>
-                                  <Card key={index} info={info}/>
-                                </div>
+                                <wednesday style={{gridRow: "" +  dict[info.start_time] + "/" + dict[info.end_time]}}>
+                                  {info.task}
+                                </wednesday>
                               )
                             }
-                          } 
+                          }
                         )
                       }
               </div>
 
               <div className = "day" style={{backgroundColor: "rgb(0, 126, 167, 0.5)"}}>
-                {      
+                {
                       this.state.thursday.map((info, index) => {
                         if((info.day !== '') && (info.task !== '') && (info.start_time !== '') && (info.end_time !== '')) {
                             return (
-                              <div style={{gridColumnStart: dict[info.start_time], gridColumnEnd: dict[info.end_time]}}>
-                                <Card key={index} info={info}/>
-                              </div>
-            
+                              <thursday style={{gridRow: "" +  dict[info.start_time] + "/" + dict[info.end_time]}}>
+                                {info.task}
+                              </thursday>
+
                             )
                           }
-                        } 
+                        }
                       )
                     }
               </div>
@@ -236,12 +236,12 @@ class App extends React.Component {
                       if((info.day !== '') && (info.task !== '') && (info.start_time !== '') && (info.end_time !== '')) {
                           console.log("dict test:" + dict[info.start_time]);
                           return (
-                            <div style={{gridColumnStart: dict[info.start_time], gridColumnEnd: dict[info.end_time]}}>
-                              <Card key={index} info={info}/>
-                            </div>
+                            <friday style={{gridRow: "" +  dict[info.start_time] + "/" + dict[info.end_time]}}>
+                              {info.task}
+                            </friday>
                           )
                         }
-                      } 
+                      }
                     )
                   }
               </div>
@@ -249,13 +249,11 @@ class App extends React.Component {
             </div>
           </div>
 
-          <div className="input-form">
-
-            <hr/>
-
-            <div className="row">
-              <form className="form-inline" onSubmit={this.onSubmit}>
-                  <select id="weekdays" name="days" ref={input => this.day = input}>
+          <div className="formPackage">
+            <form onSubmit={this.onSubmit} style={{gridColumn: "2/3"}}>
+              <div className = "thisform">
+                <div className = "form-group" style={{gridRow: "1/2", gridColumn: "1/3"}}>
+                  <select className = "data-get" id="weekdays" name="days" ref={input => this.day = input}>
                     <option value="selectcard">--- Select a Day ---</option>
                     <option value="Monday">Monday</option>
                     <option value="Tuesday">Tuesday</option>
@@ -263,44 +261,16 @@ class App extends React.Component {
                     <option value="Thursday">Thursday</option>
                     <option value="Friday">Friday</option>
                   </select>
-                  <br></br>
-                  <div>
-                    <select id="weekdays" name="days" ref={input => this.start_time = input}>
-                      <option value="selectcard">--- Select a Start Time ---</option>
+                </div>
+                <div className = "form-group" style={{gridRow: "2/3", gridColumn: "1/2"}}>
+                    <select  className = "data-get" id="weekdays" name="days"  style={{width: "1fr"}} ref={input => this.start_time = input}>
+                      <option value="selectcard">--- Start ---</option>
                       <option value="8:00AM">8:00AM</option>
                       <option value="8:30AM">8:30AM</option>
                       <option value="9:00AM">9:00AM</option>
                       <option value="9:30AM">9:30AM</option>
-                      <option value="10:00AM">10:30AM</option>
-                      <option value="10:30AM">11:00AM</option>
-                      <option value="11:00AM">11:00AM</option>
-                      <option value="11:30AM">11:30AM</option>
-                      <option value="12:00PM">12:00PM</option>
-                      <option value="12:30PM">12:30PM</option>
-                      <option value="1:00PM">1:00PM</option>
-                      <option value="1:30PM">1:30PM</option>
-                      <option value="2:00PM">2:00PM</option>
-                      <option value="2:30PM">2:30PM</option>
-                      <option value="3:00PM">3:00PM</option>
-                      <option value="3:30PM">3:30PM</option>
-                      <option value="4:00PM">4:00PM</option>
-                      <option value="4:30PM">4:30PM</option>
-                      <option value="5:00PM">5:00PM</option>
-                      <option value="5:30PM">5:30PM</option>
-                      <option value="6:00PM">6:00PM</option>
-                      <option value="6:30PM">6:30PM</option>
-                      <option value="7:00PM">7:00PM</option>
-                      <option value="7:30PM">7:30PM</option>
-                      <option value="8:00PM">8:00PM</option>
-                    </select>
-                    <select id="weekdays" name="days" ref={input => this.end_time = input}>
-                      <option value="selectcard">--- Select an End Time ---</option>
-                      <option value="8:00AM">8:00AM</option>
-                      <option value="8:30AM">8:30AM</option>
-                      <option value="9:00AM">9:00AM</option>
-                      <option value="9:30AM">9:30AM</option>
-                      <option value="10:00AM">10:30AM</option>
-                      <option value="10:30AM">11:00AM</option>
+                      <option value="10:00AM">10:00AM</option>
+                      <option value="10:30AM">10:30AM</option>
                       <option value="11:00AM">11:00AM</option>
                       <option value="11:30AM">11:30AM</option>
                       <option value="12:00PM">12:00PM</option>
@@ -322,23 +292,59 @@ class App extends React.Component {
                       <option value="8:00PM">8:00PM</option>
                     </select>
                   </div>
-                  <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="task"
-                      ref={input => this.task = input}/>
-                  </div>  
-                  
-                  <button 
-                      type="submit" 
-                      className="btn btn-primary">Submit
-                  </button>
-              </form>
-          </div>
-      <hr/>
+                  <div className = "form-group" style={{gridRow: "2/3", gridColumn: "2/3"}}>
+                    <select  className = "data-get" id="weekdays" name="days" ref={input => this.end_time = input} style={{width: "1fr"}}>
+                      <option className = "placeholder" value="selectcard">--- End ---</option>
+                      <option value="8:00AM">8:00AM</option>
+                      <option value="8:30AM">8:30AM</option>
+                      <option value="9:00AM">9:00AM</option>
+                      <option value="9:30AM">9:30AM</option>
+                      <option value="10:00AM">10:00AM</option>
+                      <option value="10:30AM">10:30AM</option>
+                      <option value="11:00AM">11:00AM</option>
+                      <option value="11:30AM">11:30AM</option>
+                      <option value="12:00PM">12:00PM</option>
+                      <option value="12:30PM">12:30PM</option>
+                      <option value="1:00PM">1:00PM</option>
+                      <option value="1:30PM">1:30PM</option>
+                      <option value="2:00PM">2:00PM</option>
+                      <option value="2:30PM">2:30PM</option>
+                      <option value="3:00PM">3:00PM</option>
+                      <option value="3:30PM">3:30PM</option>
+                      <option value="4:00PM">4:00PM</option>
+                      <option value="4:30PM">4:30PM</option>
+                      <option value="5:00PM">5:00PM</option>
+                      <option value="5:30PM">5:30PM</option>
+                      <option value="6:00PM">6:00PM</option>
+                      <option value="6:30PM">6:30PM</option>
+                      <option value="7:00PM">7:00PM</option>
+                      <option value="7:30PM">7:30PM</option>
+                      <option value="8:00PM">8:00PM</option>
+                    </select>
+                  </div>
+                  <div className = "form-group" style={{gridRow: "3/4", gridColumn: "1/3", width: "97%"}}>
+                    <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                      <input
+                        type="text"
+                        className="data-get"
+                        placeholder="Task"
+                        ref={input => this.task = input}/>
+                      </div>
+                  </div>
 
-    </div>
+                  <div className = "form-group" style={{gridRow: "4/5", gridColumn: "1/3"}}>
+                    <button
+                      className = "submit-button"
+                      type="submit"
+                      >Submit
+                      </button>
+                  </div>
+
+                  <div style={{gridColumn: "1/3", gridRow: "5/6", opacity: "0.5"}}> By: Taimur Moolji and Ethan Matheiu </div>
+                </div>
+
+              </form>
+            </div>
     </div>
       );
   }
@@ -349,12 +355,8 @@ const Card = props =>
     <div className="card">
       <div className="card mb-3">
         <div className="card-body">
-          <p className="card-title"><span>Day: </span>{props.info.day}</p>
           <p className="card-text">
-            <span>Time: </span>{props.info.start_time} - {props.info.end_time}
-          </p>
-          <p className="card-text">
-            <span>Task: </span>{props.info.task}
+            {props.info.task}
           </p>
         </div>
       </div>
